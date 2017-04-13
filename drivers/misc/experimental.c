fbacf7ca42a88e35f1a7468af8b48ea4595d1268
Jakub Biwel <jakub.biwel@nokia.com>
2017-04-13 11:05:41 +0200

    driver/misc Added completion support in order to trigger update thread

--- drivers/misc/experimental.c
+++ drivers/misc/experimental.c
@@ -15,10 +15,18 @@
 #include <linux/module.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
-
+#include <linux/completion.h>
+#include <linux/signal.h>
 #include <linux/of.h>
+#include <linux/sysfs.h>
 
 static struct task_struct *t;
+static DECLARE_COMPLETION(update_fw);
+
+static struct ex_t {
+  struct mutex dev_lock;
+};
+
 
 #ifdef CONFIG_OF
 static const struct of_device_id experimental_dt_ids[] = {
@@ -30,39 +38,112 @@ static const struct of_device_id experimental_dt_ids[] = {
 MODULE_DEVICE_TABLE(of, experimental_dt_ids);
 #endif
 
+static 	ssize_t set_trigger(struct device *dev, struct device_attribute *attr,
+			 const char *buf, size_t count) {
+
+  struct ex_t *exdev = (struct ex_t *)dev_get_drvdata(dev);
+
+  dev_info(dev, "Trigger: started\n");
+
+  complete(&update_fw);
+  msleep(1000);
+
+  dev_info(dev, "Trigger: before try lock\n");
+
+  mutex_lock(&exdev->dev_lock);
+
+  dev_info(dev, "Trigger: Mutex locked\n");
+
+  mutex_unlock(&exdev->dev_lock);
+
+  dev_info(dev, "Trigger: Mutex unlocked\n");
+
+  return count;
+}
+
+static DEVICE_ATTR(trigger, S_IWUSR, NULL, set_trigger);
+
+static const struct attribute *ex_attr[] = {
+  &dev_attr_trigger.attr,
+  NULL
+};
+
 static int updatefn(void *data) {
-
+  int ret = 0;
   struct platform_device *pdev = (struct platform_device *)data;
+  struct ex_t *exdev = (struct ex_t *)platform_get_drvdata(pdev);
 
-  dev_info(&pdev->dev, "Kthread started\n");
+  dev_info(&pdev->dev, "Update: started\n");
 
-  while(!kthread_should_stop()) {
-    msleep(100);  
+  allow_signal(SIGKILL);
+
+  ret = wait_for_completion_killable(&update_fw);
+  if (ret == -ERESTARTSYS) {
+    dev_info(&pdev->dev, "Update: canceled\n");
+    return 0;
   }
+
+  mutex_lock(&exdev->dev_lock);
+  
+  dev_info(&pdev->dev, "Update: Mutex locked - going to sleep\n");
+  msleep(5000);
+  
+  mutex_unlock(&exdev->dev_lock);
+  
+  dev_info(&pdev->dev, "Update: Mutex unlocked\n");
+
+  dev_info(&pdev->dev, "Update: complete\n");
 
   return 0;
 }
 
 static int ex_probe(struct platform_device *pdev)
 {
+  int ret = 0;
+  struct ex_t *exdev = NULL;
+
 	dev_info(&pdev->dev, "Probe for experimental driver called\n");
 
+  exdev = devm_kzalloc(&pdev->dev, sizeof(struct ex_t), GFP_KERNEL);
+  if (!exdev) {
+    dev_err(&pdev->dev, "Failed to allocate memory\n");
+    ret = -ENOMEM;
+    goto err_memalloc;
+  }
+
+  mutex_init(&exdev->dev_lock);
+
+  ret = sysfs_create_files(&pdev->dev.kobj, ex_attr);
+  if (ret)
+    goto err_sysfs;
+
+  platform_set_drvdata(pdev, exdev);
 
   t = kthread_run(&updatefn, pdev, "fw update");
   if (IS_ERR(t)) {
     dev_err(&pdev->dev, "Failed to create task\n");
-    return PTR_ERR(t);
+    ret = PTR_ERR(t);
+    goto err_thread_create;
   }
 
 	return 0;
+
+err_thread_create:
+  sysfs_remove_files(&pdev->dev.kobj, ex_attr);
+err_sysfs:
+err_memalloc:
+  return ret;
 }
 
 static int ex_remove(struct platform_device *pdev)
 {
 	dev_info(&pdev->dev, "Remove for experimental driver called\n");
 
+  sysfs_remove_files(&pdev->dev.kobj, ex_attr);
+
 	return 0;
 }
+
 
 static struct platform_driver experimental_driver = {
 	.driver		= {
